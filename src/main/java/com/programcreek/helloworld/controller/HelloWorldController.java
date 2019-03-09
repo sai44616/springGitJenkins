7069b9c8bc9f6da2c57806efaa32884d889da96e
sai44616 <45616231+sai44616@users.noreply.github.com>
2019-03-10 00:07:46 +0530

    from git

--- src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloWorldController {
 
-	String message = "Welcome to Spring MVC..!";
+	String message = "Welcome to Spring MVC.....!";
 
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
